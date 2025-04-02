b13355723c95c2dc02136784090bb4225cd2ae43
Marques Kevin <marques.developpement@gmail.com>
2025-04-02 16:02:57 +0200

    feat: add release and build components and tailwind (#1)

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import dts from "vite-plugin-dts";
+import { UserConfigExport } from "vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    lib: {
-      entry: "src/index.ts",
-      name: "SitemapyUI",
-      fileName: "index",
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+const app = (): UserConfigExport => {
+  return defineConfig({
+    plugins: [
+      react(),
+      tailwindcss(),
+      dts({
+        insertTypesEntry: true,
+      }),
+    ],
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/lib/index.ts"),
+        name: "index",
+        formats: ["es", "umd"],
+      },
+      rollupOptions: {
+        external: ["react", "react/jsx-runtime", "react-dom", "tailwindcss"],
+        output: {
+          globals: {
+            react: "React",
+            "react/jsx-runtime": "react/jsx-runtime",
+            "react-dom": "ReactDOM",
+            tailwindcss: "tailwindcss",
+          },
         },
       },
+      cssCodeSplit: false,
     },
-    cssCodeSplit: true,
-  },
-});
+  });
+};
+
+export default app;
